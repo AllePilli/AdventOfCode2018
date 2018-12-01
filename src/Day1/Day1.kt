db96f9bac9c9096205c9f0393e91cfe9dd7759b0
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-12-01 10:00:54 +0100

    Finished Day1

--- src/Day1/Day1.kt
+++ src/Day1/Day1.kt
@@ -5,5 +5,17 @@ import Functions.Companion.readlines
 class Day1{
     companion object {
         fun part1(path: String) = readlines("$path\\Day1\\input.txt").map { it.toInt() }.sum()
+        fun part2(path: String): Int{
+            val input = readlines("$path\\Day1\\input.txt").map { it.toInt() }
+            val list: MutableSet<Int> = mutableSetOf()
+            var sum = 0
+
+            while (true){
+                input.forEach {
+                    sum += it
+                    if (!list.add(sum)) return sum
+                }
+            }
+        }
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -3,5 +3,5 @@ import Day1.Day1
 val path = "C:\\Users\\alles\\OneDrive\\Documents\\KotlinProjects\\AdventOfCode2018\\src"
 
 fun main(args: Array<String>){
-    print("Day1:\t${Day1.part1(path)}")
+    print("Day1:\t${Day1.part1(path)}\t|\t${Day1.part2(path)}")
 }
