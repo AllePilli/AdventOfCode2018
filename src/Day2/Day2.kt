494b1b399fc42f226ef5b615dc55cc4ff7464335
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-12-02 11:24:39 +0100

    Finished Day 2 part 2

--- src/Day2/Day2.kt
+++ src/Day2/Day2.kt
@@ -1,16 +1,17 @@
 package Day2
 
 import Functions.Companion.readlines
+import charDifference
 import frequency
+import removeDifferences
 
 class Day2{
     companion object {
         fun part1(path: String): Int{
-            val list = readlines("$path\\Day2\\input.txt")
             var totalTwos = 0
             var totalThree = 0
 
-            list.forEach { id ->
+            readlines("$path\\Day2\\input.txt").forEach { id ->
                 var twos = false
                 var threes = false
                 id.forEach { c ->
@@ -27,6 +28,18 @@ class Day2{
             }
 
             return totalThree * totalTwos
+        }
+
+        fun part2(path: String): String{
+            val list = readlines("$path\\Day2\\input.txt")
+
+            (0 until list.size - 1).forEach { i ->
+                (i + 1 until list.size).forEach { j ->
+                    if (list[i] charDifference list[j] == 1) return list[i] removeDifferences list[j]
+                }
+            }
+
+            return ""
         }
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -5,5 +5,5 @@ val path = "C:\\Users\\alles\\OneDrive\\Documents\\KotlinProjects\\AdventOfCode2
 
 fun main(args: Array<String>){
     println("Day1:\t${Day1.part1(path)}\t|\t${Day1.part2(path)}")
-    println("Day2:\t${Day2.part1(path)}")
+    println("Day2:\t${Day2.part1(path)}\t|\t${Day2.part2(path)}")
 }
