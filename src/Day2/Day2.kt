8dc2885260bcbb7b188ac037ee83be3d342f8b9a
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-12-02 10:53:03 +0100

    Finished Day 2 part 1

--- src/Day2/Day2.kt
+++ src/Day2/Day2.kt
@@ -1,7 +1,32 @@
 package Day2
 
+import Functions.Companion.readlines
+import frequency
+
 class Day2{
     companion object {
+        fun part1(path: String): Int{
+            val list = readlines("$path\\Day2\\input.txt")
+            var totalTwos = 0
+            var totalThree = 0
 
+            list.forEach { id ->
+                var twos = false
+                var threes = false
+                id.forEach { c ->
+                    if (!twos && id.frequency(c) == 2){
+                        totalTwos++
+                        twos = true
+                    }
+
+                    if (!threes && id.frequency(c) == 3){
+                        totalThree++
+                        threes = true
+                    }
+                }
+            }
+
+            return totalThree * totalTwos
+        }
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,9 @@
 import Day1.Day1
+import Day2.Day2
 
 val path = "C:\\Users\\alles\\OneDrive\\Documents\\KotlinProjects\\AdventOfCode2018\\src"
 
 fun main(args: Array<String>){
-    print("Day1:\t${Day1.part1(path)}\t|\t${Day1.part2(path)}")
+    println("Day1:\t${Day1.part1(path)}\t|\t${Day1.part2(path)}")
+    println("Day2:\t${Day2.part1(path)}")
 }
