d918621bba3914a412dbb8401b2f1e7f0c5701ab
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-12-03 15:57:40 +0100

    Finished Day 3 part 1

--- src/Day3/Day3.kt
+++ src/Day3/Day3.kt
@@ -1,7 +1,45 @@
 package Day3
 
+import Functions.Companion.readlines
+
 class Day3{
     companion object {
+        fun part1(path: String): Int{
+            val lines = readlines("$path\\Day3\\input.txt")
+            val grid = fillGrid(lines)
+            var area = 0
 
+            grid.forEach { row -> area += row.filter { it == 'x' }.size }
+
+            return area
+        }
+
+        private fun fillGrid(lines: List<String>): MutableList<MutableList<Char>>{
+            val grid: MutableList<MutableList<Char>> = mutableListOf()
+
+            (0 until 2000).forEach { i ->
+                grid.add(mutableListOf())
+                (0 until 2000).forEach { j -> grid[i].add('.') }
+            }
+
+            lines.forEach { line ->
+                val data = line.split(" @ ")[1]
+                val co = data.split(": ")[0]
+                val size = data.split(": ")[1]
+                val x = co.split(",")[0].toInt()
+                val y = co.split(",")[1].toInt()
+                val width = size.split("x")[0].toInt()
+                val height = size.split("x")[1].toInt()
+
+                (x until x + width).forEach { i ->
+                    (y until y + height).forEach { j ->
+                        if (grid[i][j] == '.') grid[i][j] = '#'
+                        else grid[i][j] = 'x'
+                    }
+                }
+            }
+
+            return grid
+        }
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,11 @@
 import Day1.Day1
 import Day2.Day2
+import Day3.Day3
 
 val path = "C:\\Users\\alles\\OneDrive\\Documents\\KotlinProjects\\AdventOfCode2018\\src"
 
 fun main(args: Array<String>){
     println("Day1:\t${Day1.part1(path)}\t|\t${Day1.part2(path)}")
     println("Day2:\t${Day2.part1(path)}\t|\t${Day2.part2(path)}")
+    println("Day3:\t${Day3.part1(path)}")
 }
