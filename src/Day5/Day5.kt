880dee4f72e7a60f2c705610bb0eba96980f1f11
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-12-05 09:11:27 +0100

    Minor tidying in Day 5

--- src/Day5/Day5.kt
+++ src/Day5/Day5.kt
@@ -4,9 +4,16 @@ import Functions.Companion.readlines
 
 class Day5{
     companion object {
-        fun part1(path: String): Int{
-            var polymer = readlines("$path\\Day5\\input.txt")[0]
+        fun part1(path: String) = react(readlines(path)[0]).length
+
+        fun part2(path: String){
+
+        }
+
+        private fun react(polymer: String): String{
             var prevPolymer = ""
+            var poly = polymer
+
             val regList = mutableListOf<Regex>()
 
             for (c in 'a'..'z'){
@@ -14,18 +21,14 @@ class Day5{
                 regList.add(Regex("${c.toUpperCase()}$c"))
             }
 
-            while (prevPolymer != polymer){
-                prevPolymer = polymer
+            while (prevPolymer != poly){
+                prevPolymer = poly
                 regList.forEach { r ->
-                    polymer = r.replaceFirst(polymer, "")
+                    poly = r.replaceFirst(poly, "")
                 }
             }
 
-            return polymer.length
-        }
-
-        fun part2(path: String){
-
+            return poly
         }
     }
 }
