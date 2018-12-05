37722e9aae8a5f0114bd6d28af93f8979cf9c843
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-12-05 09:56:34 +0100

    Created Day 6 code skelleton

---
+++ src/Day6/Day6.kt
@@ -0,0 +1,13 @@
+package Day6
+
+class Day6{
+    companion object {
+        fun part1(path: String){
+
+        }
+
+        fun part2(path: String){
+
+        }
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,7 @@ import Day2.Day2
 import Day3.Day3
 import Day4.Day4
 import Day5.Day5
+import Day6.Day6
 
 val path = "C:\\Users\\alles\\OneDrive\\Documents\\KotlinProjects\\AdventOfCode2018\\src"
 
@@ -12,4 +13,5 @@ fun main(args: Array<String>){
     println("Day3:\t${Day3.part1(path)}\t|\t${Day3.part2(path)}")
     println("Day4:\t${Day4.part1(path)}\t|\t${Day4.part2(path)}")
     //println("Day5:\t${Day5.part1(path)}\t|\t${Day5.part2(path)}")
+    println("Day6:\t${Day6.part1(path)}\t|\t${Day6.part2(path)}")
 }
